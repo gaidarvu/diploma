35b3d5ff829afc059a74e7bbbdeb83c8b5fb6f5b
gaidar.vu <gaidar.vu@gmail.com>
2025-05-21 16:25:51 +0300

    brush code

--- terraform/main_infrastructure/outputs.tf
+++ terraform/main_infrastructure/outputs.tf
@@ -1,7 +1,7 @@
 output "worker_vm_details" {
   value = [
     for instance in yandex_compute_instance.worker_instances : {
-      name        = instance.name,
+      name            = instance.name,
       ext_ip_address  = instance.network_interface[0].nat_ip_address
       int_ip_address  = instance.network_interface[0].ip_address
     }
@@ -11,7 +11,7 @@ output "worker_vm_details" {
 output "master_vm_details" {
   value = [
     for instance in yandex_compute_instance.master_instances : {
-      name        = instance.name,
+      name            = instance.name,
       ext_ip_address  = instance.network_interface[0].nat_ip_address
       int_ip_address  = instance.network_interface[0].ip_address
     }
@@ -21,11 +21,20 @@ output "master_vm_details" {
 output "nat_vm_details" {
   value = [
     for instance in yandex_compute_instance.nat_instances : {
-      name        = instance.name,
+      name            = instance.name,
       ext_ip_address  = instance.network_interface[0].nat_ip_address
       int_ip_address  = instance.network_interface[0].ip_address
     }
   ]
 }
+
+output "load_balancer_details" {
+  value = tolist(flatten([
+    for listener in yandex_lb_network_load_balancer.web_lb.listener : [
+      for spec in listener.external_address_spec : spec.address
+    ]
+  ]))[0]
+}
+
 
 # terraform init -backend-config=backend.hcl -migrate-state
